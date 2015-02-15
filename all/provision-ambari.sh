703a7bdec69fcfa6e862929afd1ebd1bcdb1bb9a
Tim Veil <tjveil@gmail.com>
2015-02-15 14:00:30 -0500

    moving around

---
+++ all/provision-ambari.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# download HDP repos
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+
+# install ambari server and agent
+echo 'installing ambari-server and ambari-agent'
+yum install ambari-server ambari-agent -y -q
+
+# update ambari agent config
+sed -i 's/^hostname=localhost/hostname=all.hdp.local/g' /etc/ambari-agent/conf/ambari-agent.ini
+
+# setup ambari server
+ambari-server setup -s
+
+# start ambari server
+ambari-server start
+
+# start ambari agent
+ambari-agent start
---
+++ min/provision-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# clean yum
+yum clean all
+
+# update all yum libraries
+echo 'updating all libraries'
+yum update -y -q
+
+# install & start ntp as required by HDP
+echo 'installing ntp'
+yum install ntp -y -q
+service ntpd start
+
+# disable iptables as required by HDP
+echo 'disabling iptables'
+service iptables stop
+
+# disable transparent huge pages as required by HDP
+echo 'disabling transparent huge pages'
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+# disable SElinux as required by HDP
+echo 'disabling SELinux'
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
