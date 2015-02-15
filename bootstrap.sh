2fae3b532bef4f7222ad9617df333b9a06e9b74e
Tim Veil <tjveil@gmail.com>
2015-02-14 22:19:42 -0500

    adding stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,3 +39,9 @@ ambari-server start
 
 # start ambari agent
 ambari-agent start
+
+# register ambari blueprint
+curl -H "X-Requested-By: ambari" -X POST -d @/vagrant/blueprint.min.json -u admin:admin http://localhost:8080/api/v1/blueprints/min
+
+# create cluster
+curl -H "X-Requested-By: ambari" -X POST -d @/vagrant/create.min.json -u admin:admin http://localhost:8080/api/v1/clusters/min-cluster
