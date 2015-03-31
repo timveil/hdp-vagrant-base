2aa3f1b54fb5d5e26a67a4fc5940c97552758f5e
Tim Veil <tjveil@gmail.com>
2015-03-31 16:17:23 -0400

    upgrading to 2.2.4

--- repo/provision-yum.sh
+++ repo/provision-yum.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
-wget -nv http://public-repo-1.hortonworks.com/HDP/centos6/2.x/GA/2.2.0.0/hdp.repo -O /etc/yum.repos.d/HDP.repo
+wget -nv http://s3.amazonaws.com/private-repo-1.hortonworks.com/ambari/centos6/2.x/updates/27aa1558b1d5feaedb249883bf4a4aee66f3c801c55d078c0e/2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget -nv http://s3.amazonaws.com/private-repo-1.hortonworks.com/HDP/centos6/2.x/updates/37ede792e14bb5d7d2259e4fcd55a3d68dfc6fa47f4318a521745208/2.2.4.0/hdp.repo -O /etc/yum.repos.d/HDP.repo
 
 yum install yum-utils createrepo -y -q
 
@@ -12,16 +12,16 @@ chkconfig httpd on
 cd /var/www/html
 mkdir -p ambari/centos6
 cd ambari/centos6
-reposync -rq Updates-ambari-*
+reposync -r Updates-ambari-*
 
 cd /var/www/html
 mkdir -p hdp/centos6
 cd hdp/centos6
-reposync -rq HDP-*
+reposync -r HDP-*
 
-createrepo /var/www/html/hdp/centos6/HDP-2.2.0.0
+createrepo /var/www/html/hdp/centos6/HDP-2.2.4.0
 createrepo /var/www/html/hdp/centos6/HDP-UTILS-1.1.0.20
-createrepo /var/www/html/ambari/centos6/Updates-ambari-1.7.0
+createrepo /var/www/html/ambari/centos6/Updates-ambari-2.0.0
 
 cp /vagrant/ambari.repo /var/www/html/ambari/centos6
 cp /vagrant/hdp.repo /var/www/html/hdp/centos6
