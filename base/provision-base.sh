0f6f0a4a868d6dbea7c68d9ea135aefef2202a8f
Tim Veil <tjveil@gmail.com>
2015-10-15 21:21:06 -0400

    fixing iptables

--- base/provision-base.sh
+++ base/provision-base.sh
@@ -17,7 +17,9 @@ chkconfig ntpd on
 
 # disable iptables as required by HDP
 echo '******* disabling iptables'
+service iptables save
 service iptables stop
+chkconfig iptables off
 
 # disable transparent huge pages as required by HDP
 echo '******* disabling transparent huge pages'
